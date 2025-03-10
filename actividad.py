4288fa98c7e8a6bda239218aa0e6764955566923
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-03-10 12:39:24 -0300

    Update actividad.py

--- actividad.py
+++ actividad.py
@@ -5,3 +5,14 @@ palabra = input("Por favor, ingresa una palabra: ")
 
 # Mostrar la palabra ingresada
 print("La palabra ingresada es:", palabra)
+
+
+crear un programa que solicite al usuario ingresar un numero entero, si el numero es mayor a 10 mostrar por pantalla el numero 10, de lo contrario mostrar por pantalla el fin del programa
+# Solicitar al usuario que ingrese un número entero
+numero = int(input("Por favor, ingresa un número entero: "))
+
+# Comprobar si el número es mayor a 10
+if numero > 10:
+    print(10)
+else:
+    print("Fin del programa")
