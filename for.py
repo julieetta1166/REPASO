c806a1074543e57dfcf0042291104838f78d2809
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-03-10 12:45:47 -0300

    Update for.py

--- for.py
+++ for.py
@@ -1,1 +1,5 @@
 
+crear un programa que muestre los numeros del 1 al 10000, usando el ciclo for
+# Usamos un ciclo for para imprimir los números del 1 al 10000
+for i in range(1, 10001):
+    print(i)
